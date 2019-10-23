000a6f124b25642fd83d3fe8cef6ebd972ebf237
Arash Payan <arash@ara.sh>
2019-10-22 19:54:30 -0700

    Update deploment script

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,5 @@ scp *.html zood.xyz:/www/lucille/
 scp -r css zood.xyz:/www/lucille/
 scp -r libs zood.xyz:/www/lucille/
 scp -r js zood.xyz:/www/lucille/
+scp -r images zood.xyz:/www/lucille/
+scp -r fonts zood.xyz:/www/lucille/
